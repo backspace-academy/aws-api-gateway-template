42a3188012638e4ae0103d8aefbd9ea5ee32d9c3
BackSpace Academy <31625808+backspace-academy@users.noreply.github.com>
2017-09-05 18:58:01 +1000

    Uploaded completed files

---
+++ js/app.js
@@ -0,0 +1,18 @@
+
+function getPet(){
+	var apiEndpoint = "https://60hwiz4rfi.execute-api.us-east-1.amazonaws.com/Demo/pets/";
+	var petId = $('#petId').val();
+	var numPets = 3;
+	if (petId>numPets) {
+		alert("Invalid entry - exceeds number of pets");
+	}
+	else if (petId<1) {
+		alert("Invalid entry - must be greater than zero");
+	}
+	else{
+		var apiRequest = apiEndpoint + petId;
+		$.get(apiRequest, function(data, status){
+			alert("Type: " + data.type + "\nPrice: $" + data.price + "\nStatus: " + status);
+			});		
+	}
+}
