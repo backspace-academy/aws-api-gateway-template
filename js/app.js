859d0ed933f9b9c2635895c0cef510d61e23198e
BackSpace Academy <31625808+backspace-academy@users.noreply.github.com>
2017-09-06 04:26:36 +1000

    .

--- js/app.js
+++ js/app.js
@@ -1,18 +1,33 @@
+// Self-invoking anonymous function
+(function($) {
+	'use strict';
+	
+	// Click event listener
+	$('#petBtn').click(function() {
+	  getPet();
+	});
 
-function getPet(){
-	var apiEndpoint = "https://60hwiz4rfi.execute-api.us-east-1.amazonaws.com/Demo/pets/";
-	var petId = $('#petId').val();
-	var numPets = 3;
-	if (petId>numPets) {
-		alert("Invalid entry - exceeds number of pets");
-	}
-	else if (petId<1) {
-		alert("Invalid entry - must be greater than zero");
-	}
-	else{
-		var apiRequest = apiEndpoint + petId;
-		$.get(apiRequest, function(data, status){
-			alert( "\nStatus: " + status + "\nType: " + data.type + "\nPrice: $" + data.price);
-			});		
-	}
-}
+	// Send request to API
+	function getPet(){
+		// Put your endpoint here, not mine!!!!
+		var apiEndpoint = 'https://60hwiz4rfi.execute-api.us-east-1.amazonaws.com/Demo/pets/';
+		var petId = $('#petId').val();
+		var numPets = 3;
+		if (petId>numPets) {
+			alert('Invalid entry - exceeds number of pets');
+		}
+		else if (petId<1) {
+			alert('Invalid entry - must be greater than zero');
+		}
+		else{
+			// Send the request
+			var apiRequest = apiEndpoint + petId;
+			$.get(apiRequest, function(data, status){
+				// Display the response
+				alert( '\nStatus: ' + status + '\nType: ' + data.type + '\nPrice: $' + data.price);
+				});		
+		}
+	}	
+
+// End 	self-invoking anonymous function
+})(jQuery);
