db08e0631699d7d1b858476850c1e67e985e2b87
BackSpace Academy <31625808+backspace-academy@users.noreply.github.com>
2018-12-03 20:09:07 +1100

    .

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@
 	// Send request to API
 	function getPet(){
 		// Put your endpoint here, not mine!!!!
-		var apiEndpoint = 'https://60hwiz4rfi.execute-api.us-east-1.amazonaws.com/Demo/pets/';
+		var apiEndpoint = 'https://60hwiz4rfi.execute-api.us-east-1.amazonaws.com/Demo/pets';
 		var petId = $('#petId').val();
 		var numPets = 3;
 		if (petId>numPets) {
@@ -21,13 +21,13 @@
 		}
 		else{
 			// Send the request
-			var apiRequest = apiEndpoint + petId;
+			var apiRequest = apiEndpoint + "/" + petId;
 			$.get(apiRequest, function(data, status){
 				// Display the response
 				alert( '\nStatus: ' + status + '\nType: ' + data.type + '\nPrice: $' + data.price);
 				});		
 		}
-	}	
+	}
 
 // End 	self-invoking anonymous function
 })(jQuery);
